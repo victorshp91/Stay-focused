247d635eda25adc1f6293e5d1deff9a56c5d7c7e
Victor Saint Hilaire <victorshp30@gmail.com>
2025-11-06 22:37:06 -0500

    Initial Commit

---
+++ Stay focused/Stay_focusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stay_focusedApp.swift
+//  Stay focused
+//
+//  Created by Victor Saint Hilaire on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Stay_focusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
